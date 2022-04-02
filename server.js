feb21117fccee60e59d1c15fe642cdecb4ddb16a
SJoaquinR <rsanjoa@gmail.com>
2022-04-02 14:11:37 -0300

    Redes sociales passport

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const { optionsMySql } = require("./containers/utils/optionsMySql");
 const { optionsSqlite } = require("./containers/utils/optionsSqlite");
 const { faker } = require("@faker-js/faker");
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+
 const connectMongo = require("connect-mongo");
 const mongoStore = connectMongo.create({
-  // mongoUrl: "mongodb://localhost:27017/sesiones",
-  mongoUrl: "mongodb+srv://coderhouse:coderhouse@cluster0.m8qjx.mongodb.net/sesiones?retryWrites=true&w=majority",
+  mongoUrl: "mongodb://localhost:27017/sesiones",
+  //mongoUrl: "mongodb+srv://coderhouse:coderhouse@cluster0.m8qjx.mongodb.net/sesiones?retryWrites=true&w=majority",
   ttl: 600,
 });
 
@@ -31,8 +35,8 @@ const io = new Socket(httpServer);
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(
   session({
     store: mongoStore,
@@ -46,21 +50,161 @@ const productsApi = new ContainerMemory(optionsMySql);
 // const messagesApi = new containerMessage('messages.json')
 const messagesApi = new containerMessage(optionsSqlite);
 
-app.get("/", (req, res) => {
-  res.render('pages/login.ejs');
+/*============================[Middlewares]============================*/
+
+const FACEBOOK_APP_ID = "1560866814294385";
+const FACEBOOK_APP_SECRET = "c80ade890236d8cb57e8634a8382e0da";
+
+/*-------- [Conf Passport]*/
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:8080/auth/facebook/callback",
+      profileFields: ["id", "displayName", "photos", "email"],
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log("accessToken: ", accessToken);
+      console.log("refreshToken: ", refreshToken);
+      console.log(profile);
+      cb(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
 });
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+/*-------- [Passport]*/
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*============================[Base de Datos]============================*/
+const usuariosDB = [];
+
+app.get("/", (req, res) => {
+  if (req.session.nameUser) {
+    res.redirect("/datos");
+  } else {
+    res.redirect("/login");
+    //res.redirect("/auth/facebook");
+  }
+});
+
+// app.get("/", (req, res) => {
+//   res.render('pages/login.ejs');
+// });
 
 /*==================== Autenticacion ====================*/
 
-app.post("/login", (req, res) => {
-  req.session.nameUser = req.body.nameUser;
-  const nameUser = req.session?.nameUser;
+/*-------- [Passport facebook]*/
+app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    failureRedirect: "/",
+    successRedirect: "/datos",
+    authType: "reauthenticate",
+  })
+);
+/*-------- --------------------*/
 
-  if (!nameUser) {
-    res.redirect("/");
+app.get("/login", (req, res) => {
+  res.render("pages/login.ejs");
+});
+
+app.post("/login", (req, res) => {
+  const { nameUser, password } = req.body;
+
+  const existeUsuario = usuariosDB.find(
+    (usuario) => usuario.nameUser == nameUser && usuario.password == password
+  );
+  //const nameUser = req.session?.nameUser;
+
+  if (!existeUsuario) {
+    res.render("pages/login-error");
   } else {
-    res.render(path.join(process.cwd(), '/views/pages/logout.ejs'), { nameUser })
+    req.session.nameUser = nameUser;
+    req.session.password = password;
+    res.redirect("/datos");
+    // res.render(path.join(process.cwd(), "/views/pages/logout.ejs"), {
+    // nameUser,
+    //});
   }
+
+  // if (!nameUser) {
+  //   res.redirect("/");
+  // } else {
+  //   res.render(path.join(process.cwd(), '/views/pages/logout.ejs'), { nameUser })
+  // }
+});
+
+app.get("/register", (req, res) => {
+  res.render("pages/register");
+});
+
+app.post("/register", (req, res) => {
+  const { nameUser, password } = req.body;
+  const newUsuario = usuariosDB.find((usuario) => usuario.nameUser == nameUser);
+
+  if (newUsuario) {
+    res.render("pages/register-error");
+  } else {
+    usuariosDB.push({ nameUser, password });
+    res.redirect("/login");
+  }
+});
+
+app.get("/datos", (req, res) => {
+  if (req.isAuthenticated()) {
+    const datosUsuario = {
+      nombre: req.user.displayName,
+      foto: req.user.photos[0].value,
+      email: "req.user.email",
+    };
+    res.render(path.join(process.cwd(), "/views/pages/facebook.ejs"), {
+      datos: datosUsuario,
+    });
+  } else {
+    if (req.session.nameUser) {
+      const datosUsuario = usuariosDB.find((usuario) => {
+        return (
+          usuario.nameUser == req.session.nameUser &&
+          usuario.password == req.session.password
+        );
+      });
+      res.render(path.join(process.cwd(), "/views/pages/logout.ejs"), {
+        datos: datosUsuario,
+      });
+    } else {
+      console.log('USuario no autentciado')
+      res.redirect("/login");
+    }
+  }
+  
+
+  // if (req.session.nameUser) {
+  //   const datosUsuario = usuariosDB.find((usuario) => {
+  //     return (
+  //       usuario.nameUser == req.session.nameUser &&
+  //       usuario.password == req.session.password
+  //     );
+  //   });
+  //   res.render(path.join(process.cwd(), "/views/pages/logout.ejs"), {
+  //     datos: datosUsuario,
+  //   });
+  // res.render("pages/logout", {
+  //   datos: datosUsuario,
+  // });
+  // } else {
+  //   res.redirect("/login");
+  // }
 });
 
 app.get("/logout", (req, res) => {
@@ -68,17 +212,20 @@ app.get("/logout", (req, res) => {
   if (nameUser) {
     req.session.destroy((err) => {
       if (!err) {
-        res.render(path.join(process.cwd(), '/views/pages/chau.ejs'), { nameUser })
+        res.render(path.join(process.cwd(), "/views/pages/chau.ejs"), {
+          nameUser,
+        });
       } else {
-        res.redirect("/");
+        res.redirect("/login");
       }
     });
   } else {
-    res.redirect("/");
+    req.logout();
+    res.redirect("/login");
   }
 });
 
-app.get('/info', (req, res)=> {
+app.get("/info", (req, res) => {
   res.send(req.sessionID);
 });
 
